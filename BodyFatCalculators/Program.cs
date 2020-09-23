1851c89cf009113645e2dc435747acd1d980ff6a
fiakkasa <fiakkasa@live.com>
2020-09-24 01:04:48 +0300

    Remove fetch data sample code and related references

--- BodyFatCalculators/Program.cs
+++ BodyFatCalculators/Program.cs
@@ -1,7 +1,4 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BodyFatCalculators
@@ -12,8 +9,6 @@ namespace BodyFatCalculators
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync().ConfigureAwait(true);
         }
